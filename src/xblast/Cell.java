7da960add33b9bd40b9b5ba1b87ffcfdab1f6d5c
Julien Malka <julien.malka@me.com>
2016-02-22 11:31:58 +0100

    ajout d'un commentaire

--- src/xblast/Cell.java
+++ src/xblast/Cell.java
@@ -1,10 +1,18 @@
 package xblast;
 
+/**
+ * @author Yaron Dibner (257145)
+ * @author Julien Malka (259041)
+**/
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public final class Cell {
     
    public static int COLUMNS = 15;
    public static int ROWS = 13;
    public static int COUNT = COLUMNS * ROWS;
-    
 
 }
