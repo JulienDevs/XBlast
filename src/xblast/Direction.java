f5ef36fda1b51d164b8579608952f95e52bcf9b1
Youpi <yaron-96@hotmail.com>
2016-02-22 10:46:59 +0100

    First Direction.

---
+++ src/xblast/Direction.java
@@ -0,0 +1,15 @@
+package xblast;
+
+/**
+ * Direction que le joueur peut prendre.
+ * N pour nord
+ * E pour est
+ * S pour sud
+ * W pour ouest
+ * 
+ * @author Yaron Dibner (257145)
+ */
+
+public enum Direction {
+    N, E, S, W;
+}
