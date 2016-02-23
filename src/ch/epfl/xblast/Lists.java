4d4ddaba1ef7349e80ed762da20c68e52df078eb
Youpi <yaron-96@hotmail.com>
2016-02-23 15:59:47 +0100

    Board commented. Lists corrected.
    mirrored() used the method subList() wrong. subList is exclusive for the
    toIndex.

--- src/ch/epfl/xblast/Lists.java
+++ src/ch/epfl/xblast/Lists.java
@@ -22,14 +22,15 @@ public final class Lists {
      * 
      * @param List
      *          list to be mirrored
-     * @return List - symmetric version of l
+     * @return List
+     *          symmetric version of l
      * @throws IllegalArgumentException
      */
     public static <T> List<T> mirrored(List<T> l) throws IllegalArgumentException{
         if(l.isEmpty() || l == null){
             throw new IllegalArgumentException();
         }
-        List<T> tmpList = l.subList(0, l.size() - 2);
+        List<T> tmpList = l.subList(0, l.size() - 1);
         Collections.reverse(tmpList);
         l.addAll(tmpList);
         return l;
--- src/ch/epfl/xblast/server/Board.java
+++ src/ch/epfl/xblast/server/Board.java
@@ -8,18 +8,40 @@ import ch.epfl.xblast.Cell;
 import ch.epfl.xblast.Lists;
 
 /**
+ * 
+ * 
  * @author Yaron Dibner (257145)
  * @author Julien Malka (259041)
 */
 public final class Board {
     private final List<Sq<Block>> blocks;
     
+    /**
+     * Constructor of Board.
+     * @param blocks
+     *          The list of blocks that will compose the game board.
+     * @throws IllegalArgumentException
+     *          Throws IllegalArgumentException if the size of the
+     *          block list is incorrect
+     */
     public Board(List<Sq<Block>> blocks) throws IllegalArgumentException{
         if(blocks.size() != 195){
             throw new IllegalArgumentException();
         }
         this.blocks = blocks;
     }
+    
+    /**
+     * Builds a constant board with a given matrix of blocks.
+     * 
+     * @param rows
+     *          List containing the rows of blocks, which contains
+     *          a list of the columns of blocks
+     * @return board containing the blocks of rows
+     * @throws IllegalArgumentException
+     *          Throws IllegalArgumentException if the size of the
+     *          block list is incorrect
+     */
     
     public static Board ofRows(List<List<Block>> rows) throws IllegalArgumentException{
         if(rows.size() != 13){
@@ -41,6 +63,18 @@ public final class Board {
         
         return new Board(constantBlocks);
     }
+    
+    /**
+     * Builds a constant board with walls from a given matrix of the inner blocks.
+     * 
+     * @param innerBlocks
+     *          List containing the rows of the inner blocks, which contains
+     *          a list of the columns of the inner blocks
+     * @return board containing the blocks of innerBlocks and the walls
+     * @throws IllegalArgumentException
+     *          Throws IllegalArgumentException if the size of the
+     *          block list is incorrect
+     */
     
     public static Board ofInnerBlocksWalled(List<List<Block>> innerBlocks) throws IllegalArgumentException{
         if(innerBlocks.size() != 11){
@@ -67,29 +101,43 @@ public final class Board {
         return new Board(blocks);
     }
     
-    public static Board ofQuadrantNWBlocksWalled(List<List<Block>> quandrantNWBBlocks) throws IllegalArgumentException{
-        if(quandrantNWBBlocks.size() != 6){
+    /**
+     * Builds a constant board with walls from a given matrix of the north west
+     * quadrant of blocks, which will be mirrored twice to form the whole board.
+     * 
+     * @param quandrantNWBBlocks
+     *          List containing the rows of the north west quadrant, which contains
+     *          a list of the columns of the north west quadrant
+     * @return board containing the blocks of quadrantNWBlocks used to fill the
+     *          whole board and the border walls
+     * @throws IllegalArgumentException
+     *          Throws IllegalArgumentException if the size of the
+     *          block list is incorrect
+     */
+    
+    public static Board ofQuadrantNWBlocksWalled(List<List<Block>> quadrantNWBBlocks) throws IllegalArgumentException{
+        if(quadrantNWBBlocks.size() != 6){
             throw new IllegalArgumentException();
         }
-        for(int k = 0 ; k < quandrantNWBBlocks.size() ; k++){
-            if(quandrantNWBBlocks.get(k).size() != 7){
+        for(int k = 0 ; k < quadrantNWBBlocks.size() ; k++){
+            if(quadrantNWBBlocks.get(k).size() != 7){
                 throw new IllegalArgumentException();
             }
         }
         
         List<Sq<Block>> symmetricBlocks = new ArrayList<Sq<Block>>();
         
-        for(int m = 0 ; m < quandrantNWBBlocks.size() ; m++){
-            quandrantNWBBlocks.set(m, Lists.mirrored(quandrantNWBBlocks.get(m)));
+        for(int m = 0 ; m < quadrantNWBBlocks.size() ; m++){
+            quadrantNWBBlocks.set(m, Lists.mirrored(quadrantNWBBlocks.get(m)));
         }
-        quandrantNWBBlocks.addAll(Lists.mirrored(quandrantNWBBlocks));
+        quadrantNWBBlocks.addAll(Lists.mirrored(quadrantNWBBlocks));
         
-        for(int i = 0 ; i < quandrantNWBBlocks.size() + 2 ; i++){
-            for(int j = 0 ; j < quandrantNWBBlocks.get(i).size() + 2 ; j++){
-                if(i == 0 || j == 0 || i == quandrantNWBBlocks.size() + 2 || j == quandrantNWBBlocks.get(i).size() + 2){
+        for(int i = 0 ; i < quadrantNWBBlocks.size() + 2 ; i++){
+            for(int j = 0 ; j < quadrantNWBBlocks.get(i).size() + 2 ; j++){
+                if(i == 0 || j == 0 || i == quadrantNWBBlocks.size() + 2 || j == quadrantNWBBlocks.get(i).size() + 2){
                     symmetricBlocks.add(Sq.constant(Block.INDESTRUCTIBLE_WALL));
                 } else {
-                    symmetricBlocks.add(Sq.constant(quandrantNWBBlocks.get(i).get(j)));
+                    symmetricBlocks.add(Sq.constant(quadrantNWBBlocks.get(i).get(j)));
                 }
             }
         }
@@ -97,10 +145,25 @@ public final class Board {
         return new Board(symmetricBlocks);
     }
     
+    
+    /**
+     * Returns the sequence of blocks of the cell c.
+     * 
+     * @param c
+     *          Cell of which the sequence of blocks is to be returned
+     * @return sequence of blocks of c
+     */
     public Sq<Block> blocksAt(Cell c){
         return this.blocks.get(c.rowMajorIndex());
     }
     
+    /**
+     * Returns the head of the sequence of blocks of the cell c.
+     * 
+     * @param c
+     *          Cell of which the head of the sequence of blocks is to be returned
+     * @return head of the sequence of blocks of c
+     */
     public Block blockAt (Cell c){
         return this.blocksAt(c).head();
     }
