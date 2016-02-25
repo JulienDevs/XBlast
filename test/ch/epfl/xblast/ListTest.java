855758126ad01e83a9a0448bf010e121200d6738
Julien Malka <julien.malka@me.com>
2016-02-25 10:43:43 +0100

    Nouveau test pour Lists

---
+++ test/ch/epfl/xblast/ListTest.java
@@ -0,0 +1,20 @@
+package ch.epfl.xblast;
+
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * @author Yaron Dibner (257145)
+ * @author Julien Malka (259041)
+**/
+public class ListTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void mirroredMethodThrowsexceptionOnNullArgument() {
+        List <Integer> test = null;
+        Lists.mirrored(test);
+        
+    }
+
+}
