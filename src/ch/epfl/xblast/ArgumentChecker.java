95f5aeca952561e9b3ef225ff9ec5108461e0af4
Youpi <yaron-96@hotmail.com>
2016-02-29 16:41:23 +0100

    Bomb and ArgumentChecker commented.
    slt juju c rayan t tro bo <3

--- src/ch/epfl/xblast/ArgumentChecker.java
+++ src/ch/epfl/xblast/ArgumentChecker.java
@@ -1,12 +1,25 @@
 package ch.epfl.xblast;
 
 /**
+ * Immutable class.
+ * Contains a single method that checks if an integer is non negative.
+ * 
  * @author Yaron Dibner (257145)
  * @author Julien Malka (259041)
 */
 public final class ArgumentChecker {
     private ArgumentChecker(){}
     
+    /**
+     * Throws an IllegalArgumentException if a value is striclty negative. Returns
+     * value otherwise.
+     * @param value
+     *          integer to be checked
+     * @return
+     *          value is it is non negative.
+     * @throws IllegalArgumentException
+     *          if value is strictly negative.
+     */
     public static int requireNonNegative(int value) throws IllegalArgumentException{
         if(value < 0){
             throw new IllegalArgumentException();
--- src/ch/epfl/xblast/server/Bomb.java
+++ src/ch/epfl/xblast/server/Bomb.java
@@ -8,6 +8,9 @@ import ch.epfl.xblast.Cell;
 import ch.epfl.xblast.Direction;
 
 /**
+ * Immutable class.
+ * Handles the state of the bombs on the board and their explosions.
+ * 
  * @author Yaron Dibner (257145)
  * @author Julien Malka (259041)
 */
@@ -17,6 +20,23 @@ public final class Bomb {
     public final Sq<Integer> fuseLengths;
     public final int range;
     
+    /**
+     * Main constructor. Constructs a bomb given its owner,
+     * position, fuse length and range.
+     *
+     * @param ownerId
+     *          id of the player that dropped the bomb
+     * @param position
+     *          position where the bomb was dropped (a Cell)
+     * @param fuseLengths
+     *          number of ticks until the bomb will explode (current and future)
+     * @param range
+     *          range of the bomb's explosion
+     * @throws NullPointerException
+     *          if ownerId, position or fuseLenghts is null  
+     * @throws IllegalArgumentException
+     *          if fuseLengts is empty or range is strictly negative
+     */
     public Bomb(PlayerID ownerId, Cell position, Sq<Integer> fuseLengths, int range) throws NullPointerException, IllegalArgumentException{
         if(ownerId == null || position == null || fuseLengths == null){
             throw new NullPointerException();
@@ -29,27 +49,72 @@ public final class Bomb {
         this.range = ArgumentChecker.requireNonNegative(range);
     }
     
-    public Bomb(PlayerID ownerId, Cell position, int fuseLength, int range){
+    /**
+     * Auxiliary constructor that uses the main constructor. Passes a sequence
+     * of decreasing numbers of size fuseLengths and all of its other parameters.
+     * 
+     * Parameters and throws same as the main constructor, except for fuseLengths.
+     * @param ownerId
+     * @param position
+     * @param fuseLength
+     *          number of ticks until the bomb will explode (the length
+     *          of its wick)
+     * @param range
+     * @throws NullPointerException
+     * @throws IllegalArgumentException
+     */
+    public Bomb(PlayerID ownerId, Cell position, int fuseLength, int range) throws NullPointerException, IllegalArgumentException{
         this(ownerId, position, Sq.iterate(fuseLength, u -> u - 1).limit(fuseLength), range);
     }
     
+    /**
+     * @return {@code PlayerId} that dropped the bomb    
+     */ 
     public PlayerId ownerId(){ return ownerId; }
+    
+    /**
+     * @return position where the bomb was dropped
+     */
     public Cell position(){ return position; }
+    
+    /**
+     * @return sequence of fuse length of the bomb (current and future)
+     */
     public Sq<Integer> fuseLengths(){ return fuseLengths; }
+    
+    /**
+     * @return current fuse length
+     */
     public int fuseLength(){ return fuseLengths.head().intValue(); }
+    
+    /**
+     * @return range of the explosion of the bomb
+     */
     public int range(){ return range; }
     
+    /**
+     * Constructs the four arms of the explosion associated to the bomb. Its length is
+     * given by the constant Ticks.TICKS_EXPLOSION. 
+     * @return
+     */
     public List<Sq<Sq<Cell>>> explosion(){
         List<Sq<Sq<Cell>>> arms;
         
-        arms.add(explosionArmTowards(Direction.N));
-        arms.add(explosionArmTowards(Direction.E));
-        arms.add(explosionArmTowards(Direction.S));
-        arms.add(explosionArmTowards(Direction.W));
+        for(Direction dir : Direction.values()){
+            arms.add(explosionArmTowards(dir));
+        }
         
         return arms;
     }
     
+    /**
+     * Returns arm of the explosion going in the dir Direction.
+     * @param dir
+     *          Direction in which the arm will be going
+     * @return arm of the explosion as a sequence of sequence of cells,
+     *          containing the state at each tick of each particle of the arm
+     *          of the explosion.
+     */
     private Sq<Sq<Cell>> explosionArmTowards(Direction dir){
         Sq<Cell> arm = Sq.iterate(this.position, c -> c.neighbor(dir)).limit(range);
         return Sq.repeat(Ticks.EXPLOSION_TICKS, arm);
