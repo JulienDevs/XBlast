8cc9124ebc2f8cde65f30a5ca851ea83720c853f
Julien Malka <julien.malka@me.com>
2016-03-10 11:17:50 +0100

    Lol

--- src/ch/epfl/xblast/server/Ticks.java
+++ src/ch/epfl/xblast/server/Ticks.java
@@ -14,6 +14,6 @@ public interface Ticks {
     public final static int WALL_CRUMBLING_TICKS = 30;
     public final static int BONUS_DISAPPEARING_TICKS = 30;
     public final static int TICKS_PER_SECOND = 20;
-    public final static int TICK_NANOSECOND_DURATION = (1/TICKS_PER_SECOND)*Time.NS_PER_S;
-    public final static int TOTAL_TICKS = TICKS_PER_SECOND*60*2;
+    public final static int TICK_NANOSECOND_DURATION = Time.NS_PER_S/TICKS_PER_SECOND;
+    public final static int TOTAL_TICKS = TICKS_PER_SECOND*Time.S_PER_MIN*2;
 }
