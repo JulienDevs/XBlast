4cda09d6d2282462c40233078813105a399fdd44
Julien Malka <julien.malka@me.com>
2016-04-02 13:09:33 +0200

    ignore

--- src/ch/epfl/xblast/server/Ticks.java
+++ src/ch/epfl/xblast/server/Ticks.java
@@ -15,5 +15,5 @@ public interface Ticks {
     public final static int BONUS_DISAPPEARING_TICKS = 30;
     public final static int TICKS_PER_SECOND = 20;
     public final static int TICK_NANOSECOND_DURATION = Time.NS_PER_S/TICKS_PER_SECOND;
-    public final static int TOTAL_TICKS = TICKS_PER_SECOND*Time.S_PER_MIN*2;
+    public final static int TOTAL_TICKS = TICKS_PER_SECOND*Time.S_PER_MIN*4;
 }
