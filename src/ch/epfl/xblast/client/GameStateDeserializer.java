587cdb041669f1ca06bb859f664de42a66866fed
Youpi <yaron-96@hotmail.com>
2016-04-27 13:17:02 +0200

    blabla

--- src/ch/epfl/xblast/client/GameStateDeserializer.java
+++ src/ch/epfl/xblast/client/GameStateDeserializer.java
@@ -2,22 +2,44 @@ package ch.epfl.xblast.client;
 
 import java.awt.Image;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 import ch.epfl.xblast.Cell;
+import ch.epfl.xblast.PlayerID;
 import ch.epfl.xblast.RunLengthEncoder;
+import ch.epfl.xblast.client.GameState.Player;
 
 /**
  * @author Yaron Dibner (257145)
  * @author Julien Malka (259041)
  */
 public final class GameStateDeserializer {
+    private final static int NUMBER_INFORMATION_PER_PLAYER = 4;
+    private final static byte FULL_TIME_BLOCK = 21;
+    private final static byte EMPTY_TIME_BLOCK = 20;
+
     private GameStateDeserializer() {
     }
 
     public static GameState deserializeGameState(List<Byte> bytes) {
-
+        int explosionIndex = bytes.get(0) + 1;
+        int playerIndex = bytes.get(explosionIndex) + explosionIndex + 1;
+        List<Byte> bytesForBoard = bytes.subList(1, bytes.get(0) + 1);
+        List<Byte> bytesForExplosions = bytes.subList(explosionIndex + 1,
+                bytes.get(explosionIndex) + explosionIndex + 1);
+        List<Byte> bytesForPlayers = bytes.subList(playerIndex, playerIndex
+                + PlayerID.values().length * NUMBER_INFORMATION_PER_PLAYER + 1);
+        
+        List<Player> players = deserializePlayers(bytesForPlayers);
+        List<Image> board = deserializeBoard(bytesForBoard);
+        List<Image> explosions = deserializeExplosions(bytesForExplosions);
+        List<Image> score = deserializeScore(players);
+        List<Image> time = deserializeTime(bytes.get(bytes.size() - 1));
+        
+        
+        return new GameState(players, board, explosions, score, time);
     }
 
     private static List<Image> deserializeBoard(List<Byte> bytesForBoard) {
@@ -25,19 +47,38 @@ public final class GameStateDeserializer {
         ImageCollection boardImages = new ImageCollection("board");
 
         bytesForBoard = RunLengthEncoder.decode(bytesForBoard);
-        
+
         for (int i = 0; i < bytesForBoard.size(); ++i) {
             Cell c = Cell.SPIRAL_ORDER.get(i);
             int index = c.rowMajorIndex();
-            
+
             board.add(index, boardImages.image(bytesForBoard.get(i)));
         }
-        
+
         return board;
     }
-    
-    private static List<Image> deserializeExplosions(List<Byte> bytesForExplosions){
+
+    private static List<Image> deserializeExplosions(
+            List<Byte> bytesForExplosions) {
         List<Image> explosions = new ArrayList<>();
-        ImageCollection explosionImage
+        ImageCollection explosionImages = new ImageCollection("explosion");
+
+        bytesForExplosions = RunLengthEncoder.decode(bytesForExplosions);
+
+        for (byte b : bytesForExplosions) {
+            explosions.add(explosionImages.imageOrNull(b));
+        }
+
+        return explosions;
+    }
+    
+    private static List<Image> deserializeTime(byte remainingTime){
+        List<Image> time = new ArrayList<>();
+        ImageCollection scoreImages = new ImageCollection("score");
+        
+        time.addAll(Collections.nCopies(remainingTime, scoreImages.image((byte)21)));
+        time.addAll(Collections.nCopies(60 - remainingTime, scoreImages.image((byte)20)));
+        
+        return time;
     }
 }
--- src/ch/epfl/xblast/client/ImageCollection.java
+++ src/ch/epfl/xblast/client/ImageCollection.java
@@ -2,6 +2,7 @@ package ch.epfl.xblast.client;
 
 import java.awt.Image;
 import java.io.File;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -16,27 +17,32 @@ public final class ImageCollection {
     public final Map<Byte, Image> images = new HashMap<>();
 
     public ImageCollection(String dirName) {
+        File dir;
         try {
-            File dir = new File(ImageCollection.class.getClassLoader()
+            dir = new File(ImageCollection.class.getClassLoader()
                     .getResource(dirName).toURI());
-            
-            for(File file : dir.listFiles()){
-                byte byteForImage = Byte.parseByte(file.getName().substring(0, 3));
-                
-                images.put(byteForImage, ImageIO.read(file));
+
+            for (File file : dir.listFiles()) {
+                try {
+                    byte byteForImage = Byte
+                            .parseByte(file.getName().substring(0, 3));
+
+                    images.put(byteForImage, ImageIO.read(file));
+                } catch (Exception e) {
+                }
             }
-        } catch (Exception e) {
+        } catch (URISyntaxException e1) {
         }
     }
 
-    public Image image(byte byteForImage) throws NoSuchElementException{
+    public Image image(byte byteForImage) throws NoSuchElementException {
         if (!images.containsKey(byteForImage))
             throw new NoSuchElementException();
 
         return images.get(byteForImage);
     }
-    
-    public Image imageOrNull(byte byteForImage){
+
+    public Image imageOrNull(byte byteForImage) {
         return images.getOrDefault(byteForImage, null);
     }
 }
