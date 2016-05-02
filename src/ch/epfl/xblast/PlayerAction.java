c2f0533ed17016e25636d1153d17b42ed15b096e
Youpi <yaron-96@hotmail.com>
2016-05-02 16:36:58 +0200

    Ignore

--- src/ch/epfl/xblast/PlayerAction.java
+++ src/ch/epfl/xblast/PlayerAction.java
@@ -3,11 +3,9 @@ package ch.epfl.xblast;
 /**
  * @author Yaron Dibner (257145)
  * @author Julien Malka (259041)
-*/
+ */
 public enum PlayerAction {
 
     JOIN_GAME, MOVE_N, MOVE_E, MOVE_S, MOVE_W, STOP, DROP_BOMB;
-    
-    
-    
+
 }
